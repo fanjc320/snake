dcc239cc8a12e8b98d93b426782aa8db5012e36e
fanjc320 <894020798@qq.com>
2020-08-24 13:16:17 +0800

    all

---
+++ MFCApplication1View.cpp
@@ -0,0 +1,329 @@
+﻿
+// MFCApplication1View.cpp: CMFCApplication1View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFCApplication1.h"
+#endif
+
+#include "MFCApplication1Doc.h"
+#include "MFCApplication1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+#include <vector>
+#include <iostream>
+
+
+// CMFCApplication1View
+
+IMPLEMENT_DYNCREATE(CMFCApplication1View, CView)
+
+BEGIN_MESSAGE_MAP(CMFCApplication1View, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CMFCApplication1View 构造/析构
+
+CLines g_Lines;
+
+CMFCApplication1View::CMFCApplication1View() noexcept
+{
+	// TODO: 在此处添加构造代码
+	//m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+	//初始化画笔、点变量数组
+	m_pen[0].CreatePen(PS_SOLID, 1, RGB(255, 0, 0));     //红色实线，1像素宽---参数：样式、宽度、颜色
+	m_pen[1].CreatePen(PS_SOLID, 6, RGB(0, 255, 0));     //绿色实线，6像素宽
+	m_pen[2].CreatePen(PS_DASH, 1, RGB(255, 0, 0));      //红色虚线，必须为一个像素宽
+	m_pen[3].CreatePen(PS_DOT, 1, RGB(0, 0, 255));       //蓝色点线，必须为一个像素宽
+	m_pen[4].CreatePen(PS_DASHDOTDOT, 1, RGB(255, 0, 0));//红色双点虚线，必须为一个像素宽
+
+	//绘制多边形的点数组
+	m_point[0].x = 10;
+	m_point[0].y = 100;
+	m_point[1].x = 10;
+	m_point[1].y = 120;
+	m_point[2].x = 100;
+	m_point[2].y = 105;
+	m_point[3].x = 170;
+	m_point[3].y = 120;
+	m_point[4].x = 170;
+	m_point[4].y = 100;
+
+    /*AllocConsole();
+    freopen("CON", "r", stdin);
+    freopen("CON", "w", stdout);
+    freopen("CON", "w", stderr);*/
+
+    int b = 123;
+    wchar_t a[MAX_PATH] = { 0 };
+    wsprintf(a, L"%d######################################\n", b);
+    OutputDebugString(a);
+}
+
+CMFCApplication1View::~CMFCApplication1View()
+{
+}
+
+BOOL CMFCApplication1View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+void CMFCApplication1View::drawLine(CDC* pDC, CLine& line)
+{
+	if (!pDC)
+		return;
+    DrawEllipse(pDC, CPoint(line.m_begin.x, line.m_begin.y));
+    DrawEllipse(pDC, CPoint(line.m_end.x ,line.m_end.y));
+	pDC->MoveTo(line.m_begin);
+	pDC->LineTo(line.m_end);
+}
+
+void CMFCApplication1View::drawLines(CDC* pDC, std::vector<CLine>& lines)
+{
+	if (!pDC)
+		return;
+	for (auto& line : lines)
+	{
+        drawLine(pDC, line);
+	}
+}
+
+
+CLines g_headLines;
+std::vector<CPoint> g_vPoint;
+// CMFCApplication1View 绘图
+
+void CMFCApplication1View::OnDraw(CDC* pDC)
+{
+	CMFCApplication1Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+    if (!pDoc) {
+        std::cout << "err" << std::endl;
+        return;
+    }
+ 
+	
+    drawLines(pDC, g_Lines.m_lines);
+    DrawArc(pDC);
+     
+    CLine line0 = g_Lines.moveAngle(); 
+
+    /*g_vPoint.push_back(line0.m_begin); 
+    drawPoits(pDC, g_vPoint);*/
+
+    
+    //DrawAngleArc(pDC);
+    std::cout << "--" << std::endl;
+
+    wchar_t a[MAX_PATH] = { 0 };
+    wsprintf(a, L"x:%d y:%d \n", line0.m_begin.x, line0.m_begin.y);
+    OutputDebugString(a);
+
+    Sleep(100);
+    pDoc->UpdateAllViews(this);
+    Invalidate();
+
+    
+}
+
+
+
+
+// CMFCApplication1View 打印
+
+BOOL CMFCApplication1View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CMFCApplication1View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CMFCApplication1View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CMFCApplication1View 诊断
+
+#ifdef _DEBUG
+void CMFCApplication1View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCApplication1View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCApplication1Doc* CMFCApplication1View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCApplication1Doc)));
+	return (CMFCApplication1Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCApplication1View 消息处理程序
+
+//绘制直线函数
+void CMFCApplication1View::DrawLine(CDC* pDC)
+{
+    CPen* oldPen = pDC->SelectObject(&m_pen[0]);//保存DC原始画笔
+    for (int i = 0; i < 5; i++)
+    {
+        pDC->SelectObject(&m_pen[i]);        //将画笔选入DC
+        pDC->MoveTo(20, 20 + i * 15);         //设置DC当前点
+        pDC->LineTo(170, 20 + i * 15);            //绘制直线
+    }
+    pDC->SelectObject(oldPen);       //回复DC原画笔
+}
+
+
+void CMFCApplication1View::drawPoits(CDC* pDC, std::vector<CPoint> points)
+{
+    if (!pDC)
+        return;
+    if (points.size()==0)
+    {
+        return;
+    }
+    pDC->MoveTo(points[0].x,points[0].y);         //设置DC当前点
+    for (int i=1;i<points.size();++i)
+    {
+        pDC->LineTo(points[i].x,points[i].y);            //绘制直线
+    }
+}
+
+//绘制折线
+void CMFCApplication1View::DrawPolyline(CDC* pDC)
+{
+    CPen* oldPen = pDC->SelectObject(&m_pen[0]);//保存DC原始画笔
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            m_point[j].y += 35;       //每次下移35个像素
+        }
+        pDC->SelectObject(&m_pen[i]);
+        pDC->Polyline(m_point, 5);
+    }
+    pDC->SelectObject(oldPen);
+}
+
+//绘制多边形
+void CMFCApplication1View::DrawPolygon(CDC* pDC)
+{
+    for (int j = 0; j < 5; j++)
+        m_point[j].y += 75;   //数组中的点下移75个像素
+    CPen* oldPen = pDC->SelectObject(&m_pen[0]);//保存DC原始画笔
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            m_point[j].y += 35;   //每次绘制图形下移35个像素
+        }
+        pDC->SelectObject(&m_pen[i]);
+        pDC->Polygon(m_point, 5); //绘制多边形
+    }
+    pDC->SelectObject(oldPen);
+}
+
+//绘制矩形
+void CMFCApplication1View::DrawRect(CDC* pDC)
+{
+    CRect rect(150, 20, 220, 80);
+    CPen* oldPen = pDC->SelectObject(&m_pen[0]);
+    for (int i = 0; i < 5; i++)
+    {
+        rect.OffsetRect(80, 0);//每次绘图右移80个像素
+        pDC->SelectObject(&m_pen[i]);
+        pDC->Rectangle(&rect);//绘制矩形
+    }
+    pDC->SelectObject(oldPen);
+}
+
+//绘制圆角矩形
+void CMFCApplication1View::DrawRoundRect(CDC* pDC)
+{
+    CRect rect(150, 150, 220, 230);
+    CPen* oldPen = pDC->SelectObject(&m_pen[0]);
+    for (int i = 0; i < 5; i++)
+    {
+        rect.OffsetRect(80, 0);//每次绘图右移80个像素
+        pDC->SelectObject(&m_pen[i]);
+        pDC->RoundRect(&rect, CPoint(15, 15));
+    }
+    pDC->SelectObject(oldPen);
+}
+
+//绘制椭圆
+void CMFCApplication1View::DrawEllipse(CDC* pDC, CPoint mid)
+{
+    int radius = 5;
+    CRect rect(mid.x- radius, mid.y- radius, mid.x+ radius, mid.y+ radius);
+    CPen* oldPen = pDC->SelectObject(&m_pen[0]);
+    
+        rect.OffsetRect(8, 0);
+        pDC->SelectObject(&m_pen[1]);
+        pDC->Ellipse(&rect);
+     
+    pDC->SelectObject(oldPen);
+}
+
+//绘制弧线
+void CMFCApplication1View::DrawArc(CDC* pDC)
+{
+    CRect rect(100, 100, 220, 220);
+    CPoint ptStart(170, 440);
+    CPoint ptEnd(210, 400);
+    CPen* oldPen = pDC->SelectObject(&m_pen[0]);
+    for (int i = 0; i < 5; i++)
+    {
+        rect.OffsetRect(180, 0);
+        ptStart += CPoint(80, 0);
+        ptEnd += CPoint(80, 0);
+        pDC->SelectObject(&m_pen[i]);
+        pDC->Arc(&rect, ptStart, ptEnd);
+    }
+    pDC->SelectObject(oldPen);
+}
+
+//绘制直线与弧线
+void CMFCApplication1View::DrawAngleArc(CDC* pDC)
+{
+    CRect rect(150, 500, 220, 560);
+    CPoint ptCenter(185, 600);
+    CPoint ptStart(150, 630);
+    int r = 30;
+    float angleStart = 0;
+    float angleEnd = 180;
+    CPen* oldPen = pDC->SelectObject(&m_pen[0]);
+    for (int i = 0; i < 5; i++)
+    {
+        rect.OffsetRect(80, 0);
+        ptStart += CPoint(80, 0);
+        pDC->MoveTo(ptStart);
+        pDC->SelectObject(&m_pen[i]);
+        pDC->AngleArc(ptCenter.x, ptCenter.y, r, angleStart, angleEnd);
+    }
+    pDC->SelectObject(oldPen);
+}
---
+++ MFCApplication1View.h
@@ -0,0 +1,328 @@
+﻿
+// MFCApplication1View.h: CMFCApplication1View 类的接口
+//
+
+#pragma once
+#include <vector>
+#include <cmath>
+
+struct CMyPoint
+{
+	float m_x;
+	float m_y;
+};
+struct CUnitAngle
+{
+	std::vector<CMyPoint> m_vPoint;
+	void getPoints(int iNum)
+	{
+		float angle = 360.0f / iNum;
+		for (int i=0;i<iNum;++i)
+		{
+			float x = cos(angle*i / 180 * 3.1415926);
+			float y = sin(angle*i / 180 * 3.1415926);
+			CMyPoint p;
+			p.m_x = x;
+			p.m_y = y;
+			m_vPoint.push_back(p);
+		}
+	}
+	void printPoints()
+	{
+		for (auto p : m_vPoint)
+		{
+			//std::cout << p.m_x << p.m_y << std::endl;
+		}
+	}
+};
+
+
+
+//单位向量，用来表示角度
+struct CAngle
+{
+	float distancePoint(float x1, float y1, float x2, float y2)
+	{
+		//return  pow(pow(x1 - x2, 2) + pow(y1 - y2, 2), 0.5);
+		float all = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+		float s =sqrt(all);//2次开根bai号函数,必须du包含头文件zhicmath.
+		return s;
+	}
+
+	CAngle(float x, float y)//输入目标向量
+	{
+		//int distance = (int)distancePoint(0, 0, x, y);
+		float distance = distancePoint(x, y, 0, 0);
+		if (distance < 0.0001)
+		{
+			distance = 0.0001;
+		}
+		//求差向量
+		x -= distance;
+		distance = distancePoint(x, y, 0, 0);
+		if (distance < 0.0001)
+		{
+			distance = 0.0001;
+		}
+		m_x = x  / distance;
+		m_y = y  / distance;
+		 
+	}
+	/*CAngle(CPoint point)
+	{
+		CAngle(point.x, point.y);
+	}*/
+	float m_x = 1;
+	float m_y = 0;
+};
+
+struct CVec// 在angle的基础上加上长度
+{
+	CAngle m_angle = CAngle(1, 0);
+	//CAngle m_angle(1,0);
+	float m_fLength = 0;
+	CPoint m_ang_point = CPoint(1, 1);
+	float m_x = 0;
+	float m_y = 0;
+
+	float distancePoint(float x1, float y1, float x2, float y2)
+	{
+		return  pow(pow(x1 - x2, 2) + pow(y1 - y2, 2), 0.5);
+	}
+	CVec(float x , float y, float fLength)
+	{
+		m_angle = CAngle(x, y);
+		m_fLength = fLength;
+		
+		m_x = m_angle.m_x * fLength;
+		m_y = m_angle.m_y * fLength;
+	}
+	CVec(CAngle angle, float fLength)
+	{
+		CVec(angle.m_x,angle.m_y, fLength);
+	}
+	void setXY(CAngle angle, float fLength)
+	{
+		m_angle = angle;
+		m_x = m_angle.m_x * fLength;
+		m_y = m_angle.m_y * fLength;
+	}
+	/*CVec(float x , float y)
+	{
+		m_x = x;
+		m_y = y;
+		m_angle = CAngle(x,y);
+		m_fLength = distancePoint(0, 0, x, y);
+	}*/
+
+	
+};
+
+static int g_Id = 0;
+
+struct CLine {
+	CLine(CPoint begin, CPoint end) {
+		m_begin = begin;
+		m_end = end;
+		m_mid = CPoint((m_begin.x + m_end.x) / 2, (m_end.y + m_end.y) / 2);
+		m_unitAngle.getPoints(12);
+		++g_Id;
+
+	}
+	CLine()
+	{
+		m_unitAngle.getPoints(12);
+		++g_Id;
+	}
+
+	CPoint m_begin;
+	CPoint m_end;
+	CPoint m_mid;
+	CAngle m_moveAng = CAngle(1.0f,0.0f);
+	CUnitAngle m_unitAngle;
+	int m_angleCnt = 0;
+	
+
+	void moveTo(CPoint point)
+	{
+		m_mid += point;
+		m_begin += point;
+		m_end += point;
+	}
+
+	//CLine moveAngle(CAngle angle, int iDistance)
+	void moveAngle(CAngle angle, int iDistance)
+	{
+		//m_moveAng = CAngle(1.0f, 0.0f);
+		CAngle newAng = CAngle(m_moveAng.m_x+angle.m_x, m_moveAng.m_y+angle.m_y);
+		m_moveAng = newAng;
+
+		CString str;
+		float length = m_moveAng.m_x* m_moveAng.m_x + m_moveAng.m_y * m_moveAng.m_y;
+		str.Format(_T("moveAngle id:%d x:%6f y:%6f length:%6f angle_x:%6f angle_y:%6f\n"), 
+			g_Id, m_moveAng.m_x, m_moveAng.m_y ,length, angle.m_x, angle.m_y);
+		//OutputDebugString(str);
+
+		int size = m_unitAngle.m_vPoint.size();
+		int cnt = m_angleCnt % size;
+		CMyPoint mp = m_unitAngle.m_vPoint[cnt];//一个单位向量，表示方向
+
+		str.Format(_T("moveAngle cnt:%d x:%6f y:%6f \n"),
+			cnt, mp.m_x, mp.m_y);
+		OutputDebugString(str);
+		++m_angleCnt;
+		CVec vv(mp.m_x,mp.m_y, iDistance);
+		moveVec(vv);
+
+		/*CVec vec(m_moveAng, iDistance);
+		CVec vec(m_moveAng,iDistance);
+		vec.setXY(m_moveAng,iDistance);
+		moveVec(vec);*/
+		//return CLine(m_begin, m_end);
+
+		
+	}
+	//在angle的基础上加上长度
+	//CLine moveVec(CVec vec)
+	void moveVec(CVec vec)
+	{
+		CString str;
+		//str.Format(_T("moveVec x:%6f y:%6f\n"), vec.m_x, vec.m_y);
+		OutputDebugString(str);
+
+		m_mid.x += vec.m_x;
+		m_begin.x += vec.m_x;
+		m_end.x += vec.m_x;
+
+		int yy = (int)round(vec.m_y);
+		m_mid.y += yy;
+		m_begin.y += yy;
+		m_end.y += yy;
+		//return CLine(m_begin, m_end);
+	}
+
+	void moveRight(int iDistance)
+	{
+		m_mid.x += iDistance;
+		m_begin.x += iDistance;
+		m_end.x += iDistance;
+	}
+};
+
+struct CLines {
+	CLines()
+	{
+		CPoint begin(200, 200);
+		int iLength = 50;
+		CPoint end = begin;
+		for (int i = 0; i < 10; ++i)
+		{
+			CPoint end(begin.x - iLength, begin.y);
+			CLine line(begin, end);
+			m_lines.push_back(line);
+			begin = end;
+		}
+	}
+
+	std::vector<CLine> m_lines;
+	
+	void moveRight(int iDistance)
+	{
+		for (auto& line : m_lines)
+		{
+			line.moveRight(iDistance);
+		}
+	}
+
+	void addLine(CLine line)
+	{
+		m_lines.push_back(line);
+	}
+
+	CLine moveAngle()
+	{
+		int distance = m_distance;
+		CAngle angle = CAngle(10, 1);
+		CLine& line0 = m_lines[0];
+		for (int i=0; i < m_lines.size(); ++i)
+		{
+			CLine& line = m_lines[i];
+			//CLine newLine = line.moveAngle(angle, distance);
+			line.moveAngle(angle, distance);
+			/*
+			CPoint angPoint = (newLine.m_mid.x - line.m_mid.x , newLine.m_mid.y - line.m_mid.y);
+			//angle = CAngle(angPoint.x, angPoint.y);
+			if (i==0)
+			{
+				line0 = newLine;
+			}*/
+			
+		}
+		return line0;
+	}
+	
+	CAngle m_angle = CAngle(10, 1);
+	int m_distance = 10;
+};
+
+class CMFCApplication1View : public CView
+{
+protected: // 仅从序列化创建
+	CMFCApplication1View() noexcept;
+	DECLARE_DYNCREATE(CMFCApplication1View)
+
+// 特性
+public:
+	CMFCApplication1Doc* GetDocument() const;
+
+// 操作
+public:
+
+	CPen m_pen[5];
+	CPoint m_point[5];
+	void DrawLine(CDC* pDC);
+	void drawPoits(CDC* pDC, std::vector<CPoint> points);
+	void DrawPolyline(CDC* pDC);
+	void DrawPolygon(CDC* pDC);
+	void DrawRect(CDC* pDC);
+	void DrawRoundRect(CDC* pDC);
+	//void DrawEllipse(CDC* pDC);
+	void DrawArc(CDC* pDC);
+	void DrawAngleArc(CDC* pDC);
+
+	void DrawEllipse(CDC* pDC, CPoint mid);
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	void drawLine(CDC* pDC, CLine& line);
+	void drawLines(CDC* pDC, std::vector<CLine>& lines);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMFCApplication1View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+//void initLines(std::vector<CLine>& lines);
+void initLines();
+
+#ifndef _DEBUG  // MFCApplication1View.cpp 中的调试版本
+inline CMFCApplication1Doc* CMFCApplication1View::GetDocument() const
+   { return reinterpret_cast<CMFCApplication1Doc*>(m_pDocument); }
+#endif
+
